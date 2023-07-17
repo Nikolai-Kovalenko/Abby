3d4f338202d495134831e94e66c25e20caaeecca
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-07-17 18:11:45 +0300

    first commit

---
+++ Abby/AbbyWeb/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace AbbyWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public string DisplayOrder { get; set; }
+    }
+}
