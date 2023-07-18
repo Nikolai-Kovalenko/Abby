24c9059dcd64120f5fea1f87b6e5062078602529
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-07-18 23:00:56 +0300

    add first migration

---
+++ Abby/AbbyWeb/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AbbyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbyWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Category { get; set; }     
+    }
+}
--- Abby/AbbyWeb/Models/Category.cs
+++ Abby/AbbyWeb/Models/Category.cs
@@ -1,9 +1,14 @@
-﻿namespace AbbyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string  Name { get; set; }
-        public string DisplayOrder { get; set; }
+        public string DisplayOrder { get; set; } 
     }
 }
