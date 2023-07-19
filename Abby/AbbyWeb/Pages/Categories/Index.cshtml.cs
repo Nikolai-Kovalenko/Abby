eea18502ccf74e4fcc8351d4c023cb29015c2e9f
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2023-07-19 23:32:04 +0300

    add forms for create category

--- Abby/AbbyWeb/Pages/Categories/Index.cshtml.cs
+++ Abby/AbbyWeb/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using AbbyWeb.Data;
+using AbbyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,18 @@ namespace AbbyWeb.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly AppDbContext _db;
+
+        public IEnumerable<Category> Categories { get; set; } 
+
+        public IndexModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            Categories = _db.Category;
         }
     }
 }
